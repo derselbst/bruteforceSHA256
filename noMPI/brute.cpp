a9fda609a80029ea065b5612ea9a819a1b065d74
derselbst <tom.mbrt@googlemail.com>
2014-10-11 17:46:09 +0200

    added lots of comments

--- noMPI/brute.cpp
+++ noMPI/brute.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib> // exit()
 #include <cstdio> // printf()
-#include <cstring> // strcmp()
+#include <cstring> // memcmp()
 #include <string>
 #include <stack>
 #include <openssl/sha.h>
@@ -10,14 +10,26 @@
 
 using namespace std;
 
+// clear text password entered by user
 string pwd;
 
+// contains the hash of the unknown password
 char pwdHash[SHA256_DIGEST_LENGTH];
+
+// contains the hash of a bruteforced string
 char bruteHash[SHA256_DIGEST_LENGTH];
 
+// the maximum number of characters bruteforce shall check
 static const unsigned char MaxChars = 3;
 
-void printSHAHash(unsigned int* pbuf)
+/**
+ * @brief prints 32 bytes of memory
+ *
+ * prints a hex dump of 32 bytes of memory pointed to
+ *
+ * @param[in]   pbuf: pointer to some memory, usually containing an SHA256 hash
+ */
+void printSHAHash(const unsigned int *const pbuf)
 {
     // byteswap the integer pointed to, to display hex dump in correct order
     // TODO: how to deal with big endian machines
@@ -33,15 +45,31 @@ void printSHAHash(unsigned int* pbuf)
           );
 }
 
-bool generateSHA256(const void *const inputStr, const size_t &length, char *const hashStr)
+/**
+ * @brief generates an SHA256 hash
+ *
+ * generates an SHA256 hash using openSSL
+ *
+ * @param[in]      input:   a const pointer to const block of data, usually a char array of which the hash is being generated
+ * @param[in]      length:  the number of bytes the that input points to holds
+ * @param[in,out]  hashStr: const pointer to an array of SHA256_DIGEST_LENGTH bytes that will receive the hash
+ *
+ * @return returns true if the hash has been generated successfully; returns false if input or hashStr is NULL or length==0; else: false
+ */
+bool generateSHA256(const void *const input, const size_t &length, char *const hashStr)
 {
+    if(!hashStr || !input || length==0)
+    {
+        return false;
+    }
+
     SHA256_CTX hash;
     if(!SHA256_Init(&hash))
     {
         return false;
     }
 
-    if(!SHA256_Update(&hash, inputStr, length))
+    if(!SHA256_Update(&hash, input, length))
     {
         return false;
     }
@@ -54,6 +82,14 @@ bool generateSHA256(const void *const inputStr, const size_t &length, char *cons
     return true;
 }
 
+/**
+ * @brief checks equality of two hashes
+ *
+ * calculates the SHA256 hash of 'password' and compares it
+ * with the initial password hash
+ *
+ * @param[in]   password: a const string containing a guessed password
+ */
 void checkPassword(const string &password)
 {
 //#ifdef VERBOSE
@@ -75,6 +111,15 @@ void checkPassword(const string &password)
     }
 }
 
+/**
+ * @brief recursive implementation of bruteforce
+ *
+ * recursive implementation of bruteforce attack
+ * call it as follows: bruteRecursive(string(""), width);
+ *
+ * @param[in]   baseString: a const string indicates the prefix of a string to be checked
+ * @param[in]   width:      the maximum number of characters you wish to be checked
+ */
 void bruteRecursive(const string baseString, const unsigned int width)
 {
     for(int i=0; i<SizeAlphabet; i++)
@@ -88,6 +133,14 @@ void bruteRecursive(const string baseString, const unsigned int width)
     }
 }
 
+/**
+ * @brief iterative implementation of bruteforce
+ *
+ * iterative implementation of bruteforce attack
+ * call it as follows: bruteIterative(width);
+ *
+ * @param[in]   width:      the maximum number of characters you wish to be checked
+ */
 void bruteIterative(const unsigned int width)
 {
     stack<string> myStack;
