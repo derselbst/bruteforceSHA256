a222537996500919c90a96ebf62e6e53437a353d
derselbst <tom.mbrt@googlemail.com>
2014-10-24 20:29:18 +0200

    noMPI: ask user for password or hash

--- noMPI/test.cpp
+++ noMPI/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <openssl/sha.h>
 #include <assert.h>
 
@@ -11,13 +12,29 @@ extern volatile bool strFound;
 
 int main()
 {
-    string pwd="<,6F";
+    cout << "Enter a Password or an SHA256 Hash: ";
 
-    // generate sha hash from entered string and write it to pwdHash
-    if(!generateSHA256(pwd.c_str(), pwd.length(), pwdHash))
+    string pwd;
+    cin >> pwd;
+
+    while((pwd.length() > MaxChars) && (pwd.length() != SHA256_DIGEST_LENGTH))
     {
-        cerr << "Error when generating SHA256 from \"" << pwd << "\"" << endl;
-        return -2;
+        cerr << "Error: The Password you entered must be shorter " << (int)MaxChars << " Characters." << endl << "Try Again: ";
+        cin >> pwd;
+    }
+
+    if(pwd.length() == SHA256_DIGEST_LENGTH)
+    {
+        memcpy(pwdHash, pwd.c_str(), SHA256_DIGEST_LENGTH);
+    }
+    else
+    {
+        // initialize the hash buffer for the password
+        if(!generateSHA256(pwd.c_str(), pwd.length(), pwdHash))
+        {
+            cerr << "Error when generating SHA256 from \"" << pwd << "\"" << endl;
+            return -2;
+        }
     }
 
     cout << "checking using Recusive Method" << endl;
